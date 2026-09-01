a997901601fe590b6ef2d66e557b04e18648b566
dannas-3989 <na5407050@gmail.com>
2026-09-01 07:04:06 +0300

    fix(api): point baseURL to team1-api.primetrustx.com per backend team

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-// Base Axios instance
+
 const API = axios.create({
-  baseURL: 'https://lock-dep-back.onrender.com/api',
+  baseURL: 'https://team1-api.primetrustx.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Interceptor to attach JWT token if present in LocalStorage
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
